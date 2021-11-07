4da5834ea1484d0125579d7d2d823c1a85851600
SimonImanigirimpuhwe <softimag08@gmail.com>
2021-11-07 09:34:57 +0200

    update card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,7 +5,9 @@ const Card = ({image}) => {
     return ( 
         <div className="card">
             <img src={image} alt={image} />
-            <p>This is the card component</p>
+            <div>
+                <p>This is the card component</p>
+            </div>
         </div>
      );
 }
